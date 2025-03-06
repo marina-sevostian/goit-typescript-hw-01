ba4916b074d5f870817b760d98997b3ee7e4a58b
Marina.S. <marina.sevostian@gmail.com>
2025-03-06 06:15:41 +0200

    fixed

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get<T>(url);
     return response.data;
-  } catch (error) {
+  } catch (error: unknown) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
 }
